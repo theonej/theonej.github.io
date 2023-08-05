c0c776b391dbc4ff107cb3d66b4c6dc325e4345a
Jason Henry <jasonhenry@Jasons-Mac-mini.fios-router.home>
2023-08-04 21:45:16 -0400

    added __pycache__ to gitignore and removed them from git

--- code/text-embeddings-similarity-search/server.py
+++ code/text-embeddings-similarity-search/server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from type_definitions.document_info import DocumentInfo
 from handlers.document_command_handlers import DocumentCommandHandlers
-import json
 
 app = FastAPI()
 
@@ -9,13 +8,7 @@ app = FastAPI()
 def index_document(document:DocumentInfo):
     handler = DocumentCommandHandlers()
 
-    print(document)
-    
     embeddings = handler.handle_index_document_command(document)
     print(embeddings)
     
     return {"embeddings":embeddings.tolist()}
-
-@app.get("/health-check")
-def health_check():
-    return True
